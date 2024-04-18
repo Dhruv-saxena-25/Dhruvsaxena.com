e564d2d585b20a986b1c25c496242758582ea77e
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-04-18 22:08:07 +0100

    commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Dhruvsaxena.com
+
+
+
+<!-- # [johnppinto.github.io](https://johnppinto.github.io/) -->
+
+# Data Science Portfolio by Dhruv Saxena
+
+This is my personal portfolio containing a compilation of notebooks on different projects in the field of data science. This portfolio includes projects that I have researched out of interest and curiosity during my learning in the subject of data analysis and machine learning.
---
+++ server.py
@@ -0,0 +1,59 @@
+import csv
+import git
+import datetime
+from zoneinfo import ZoneInfo
+from flask import Flask, render_template, request, Response
+
+app = Flask(import_name=__name__)
+
+# Github webhook for updating the server when pushed to github
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./Dhruvsaxena.com')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated Portfolio successfully', 200
+    else:
+        return 'Something went wrong. Try again!', 400
+
+@app.route('/')
+def index_page():
+    return render_template(template_name_or_list='index.html')
+
+# App route for multiple html pages
+# @app.route('/<string:page_name>')
+# def html_page():
+#     return render_template(template_name_or_list=page_name)
+
+# Adding data to database in CSV file
+def write_to_database(data):
+    with open(file='database.csv', mode='a', newline='') as csv_file:
+        date_time = datetime.datetime.now(tz=ZoneInfo('Asia/Kolkata'))
+        date_time = date_time.strftime('%Y-%m-%d %H:%M:%S')
+        name = data.get('name')
+        email = data.get('email')
+        message = data.get('message')
+        csv_writer = csv.writer(csv_file, 
+                                delimiter=',', 
+                                quotechar='"', 
+                                quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([date_time, name, email, message])
+
+# App route for form submission and saving form data to database
+@app.route('/submit-form', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_database(data=data)
+            return Response(status=204)
+        except Exception as e:
+            return f'Something went wrong. Try again! {e}'
+    else:
+        return 'Something went wrong. Try again!', 400
+
+# if __name__ == '__main__':
+#     app.jinja_env.auto_reload = True
+#     app.config['TEMPLATES_AUTO_RELOAD'] = True
+#     app.run(debug=True)
